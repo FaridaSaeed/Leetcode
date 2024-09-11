91aebdadb96ad43472d6bb20537a0209655148a0
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2024-09-11 17:25:21 +0300

    Time: 21 ms (22.53%) | Memory: 11 MB (84.68%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,22 +1,26 @@
 const int N = 505;
 int mem[N][N];
+int n,m;
 string s1,s2;
-int dp(int n, int m) {
-    if (!n || !m) return n + m;
-    int &ret = mem[n][m];
-    if (~ret) return ret;
-    ret = (s1[n - 1] != s2[m - 1]) + dp(n - 1, m - 1);
-    ret = min(ret, 1 + dp(n - 1, m));
-    ret = min(ret, 1 + dp(n, m - 1));
+int dp(int i,int j)
+{
+    if (i == n) return m - j; 
+    if (j == m) return n - i;
+    int &ret = mem[i][j];
+    if(~ret)return ret;
+    ret = (s1[i]!=s2[j])+dp(i+1,j+1);
+    ret = min(ret,1+dp(i+1,j));
+    ret = min(ret,1+dp(i,j+1));
+    
     return ret;
 }
 class Solution {
 public:
     int minDistance(string word1, string word2) {
         s1 = word1, s2 = word2;
-        int n = word1.size();
-        int m = word2.size();
+        n = word1.size();
+        m = word2.size();
         memset(mem,-1,sizeof mem);
-        return dp(n,m);
+        return dp(0,0);
     }
 };
