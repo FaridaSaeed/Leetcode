e36788ceed04f940b693d2a0c2c5fc2dd7c48df6
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2024-09-30 16:14:27 +0300

    Time: 7 ms (59.15%) | Memory: 10.1 MB (80.66%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        unordered_map<int,string>mp;
-        mp[1] = "I";
-        mp[4] = "IV";
-        mp[5] = "V";
-        mp[9] = "IX";
-        mp[10] = "X";
-        mp[40] = "XL";
-        mp[50] = "L";
-        mp[90] = "XC";
-        mp[100] = "C";
-        mp[400] = "CD";
-        mp[500] = "D";
-        mp[900] = "CM";
-        mp[1000] = "M";
-        vector<int>v = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
-        string ans;
-        int n = v.size();
-        for(int i=n-1;i>=0;i--)
-        {
-            while(num>=v[i])
-            {
-                ans+=mp[v[i]];
-                num-=v[i];
-            }
-        }
-        return ans;
-        
+    int nums[13]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    string s[13]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    int i=0;
+    string ret="";
+    while(num!=0){
+       if(num>=nums[i]){
+            num=num-nums[i];
+            ret+=s[i];
+       }
+       else{
+        i++;
+       }
     }
-};
+    return ret;
+    }
+}; 
