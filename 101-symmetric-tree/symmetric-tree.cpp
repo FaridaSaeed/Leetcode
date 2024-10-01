3cccc25650420ecb84c4692dcb58ddf7ffb8ca8b
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2024-10-01 20:53:13 +0300

    Time: 4 ms (50.49%) | Memory: 17.6 MB (68.64%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -19,8 +19,6 @@ public:
         return min(isSym(root1->left,root2->right),isSym(root1->right,root2->left));
     }
     bool isSymmetric(TreeNode* root) {
-        if(root->left==NULL && root->right==NULL)return true;
-        if(root->left==NULL || root->right==NULL)return false;
         return isSym(root->left,root->right);
     }
 };
