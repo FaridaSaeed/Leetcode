649e232bdb634fa78256b334a04286991d8a114d
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-01-05 16:17:26 +0200

    Time: 0 ms (100.00%) | Memory: 27.1 MB (25.91%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size(), max_pre = 0, max_suff = 0, ans = 0;
-        vector<int> pre(n), suff(n);
-        for(int i = 0 ; i < n ; i++){
-            pre[i] = max_pre;
-            max_pre = max(max_pre, height[i]);
-        }
-        for(int i = n - 1 ; i >= 0 ; i--){
-            suff[i] = max_suff;
-            max_suff = max(max_suff, height[i]);
-            ans += max(0, min(suff[i], pre[i]) - height[i]);
+        stack<int>st;
+        int n = height.size();
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && height[i]>=height[st.top()])
+            {
+                int ind = st.top();
+                st.pop();
+                if(!st.empty())
+                {
+                    ans+=(i-st.top()-1)*(min(height[st.top()],height[i])-height[ind]);
+                }
+            }
+            st.push(i);
         }
         return ans;
     }
