33e8c38f824123ea26ead3b23e92844c9f4347c8
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-01-07 13:18:07 +0200

    Time: 473 ms (39.99%) | Memory: 317.6 MB (5.02%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,24 +1,23 @@
+const int N = 3*1e5;
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        vector<int>fre(2*1e5);
-        unordered_map<int,int>mp;
+        vector<int>freq(N);
         int mx = 0;
-        int mxele = 0;
-        for(auto i:nums){
-            if(i-k<0)fre[0]++;
-            else 
-                fre[i-k]++;
-            fre[i+k+1]--;
-            mp[i]++;
-            mxele = max(mxele,i+k+1);
-            }
-
-        for(int i=1;i<=mxele;i++)
+        unordered_map<int,int>mp;
+        for(auto i:nums)
         {
-            fre[i]+=fre[i-1];
-            mx = max(mx,min(fre[i],mp[i]+numOperations));
+            mp[i]++;
+            if(i-k<0)freq[0]++;
+            else freq[i-k]++;
+            freq[i+k+1]--;
+            mx = max(mx,i+k);
         }
-        return mx;
+        int ans = 0;
+        for(int i=1;i<=mx;i++){
+            freq[i]+=freq[i-1];
+            ans = max(ans,mp[i]+min(freq[i]-mp[i],numOperations));
+        }
+        return ans;
     }
 };
