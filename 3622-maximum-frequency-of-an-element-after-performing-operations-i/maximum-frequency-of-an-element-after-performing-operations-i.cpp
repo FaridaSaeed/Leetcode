6011b9d635b455ae7a16faace938dcb43cbd6ac9
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-01-07 13:43:34 +0200

    Time: 470 ms (40.21%) | Memory: 317.5 MB (5.02%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -2,22 +2,21 @@ const int N = 3*1e5;
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        map<int,int>mp,freq;
+        vector<int>freq(N);
+        int mx = 0;
+        unordered_map<int,int>mp;
         for(auto i:nums)
         {
             mp[i]++;
             if(i-k<0)freq[0]++;
             else freq[i-k]++;
             freq[i+k+1]--;
-            freq[i] += 0;
+            mx = max(mx,i+k);
         }
         int ans = 0;
-        int lst = 0;
-        for(auto i:freq)
-        {
-            i.second+=lst;
-            ans = max(ans,min(i.second,mp[i.first]+numOperations));
-            lst = i.second;
+        for(int i=1;i<=mx;i++){
+            freq[i]+=freq[i-1];
+            ans = max(ans,min(freq[i],mp[i]+numOperations));
         }
         return ans;
     }
