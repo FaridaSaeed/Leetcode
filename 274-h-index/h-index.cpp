c0704f942538b41b801c0e7a7407f93419ae873f
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-01-07 18:21:43 +0200

    Time: 0 ms (100.00%) | Memory: 12.3 MB (27.16%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int hIndex(vector<int>& c) {
         sort(c.begin(),c.end());
-        int mx = 0,n = c.size();
+        int n = c.size();
         for(int i =0;i<n;i++)
         {
-            if(n-i>=c[i])mx = max(mx,c[i]);
-            else mx = max(mx,n-i);
+            if(n-i<=c[i])return n-i; 
         }
-        return mx;
+        return 0;
     }
 };
