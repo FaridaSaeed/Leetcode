617505fe1960ef31ecf5a2a32f0b02de82606c6f
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-03-26 21:35:14 +0200

    Time: 2 ms (17.50%) | Memory: 9.5 MB (27.07%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    void help(int n,string& digits,int idx,vector<string>&ans,string &str,map<int,string>&ma){
-        if(idx==n){
-            ans.push_back(str);
+    vector<string>ans;
+    string s;
+    int n;
+    unordered_map<char,string>mp;
+    void rec(string d,int ind)
+    {
+        if(ind>=n){
+            ans.push_back(s);
             return;
         }
-        string strr = ma[digits[idx]-'0'];
-        for(int i=0;i<strr.size();i++){
-            str.push_back(strr[i]);
-            help(n,digits,idx+1,ans,str,ma);
-            str.pop_back();
+        for(auto c:mp[d[ind]])
+        {
+            s+=c;
+            rec(d,ind+1);
+            s.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        if(digits.size()==0) return{};
-        map<int,string>ma;
-        ma[2]="abc";
-        ma[3]="def";
-        ma[4]="ghi";
-        ma[5]="jkl";
-        ma[6]="mno";
-        ma[7]="pqrs";
-        ma[8]="tuv";
-        ma[9]="wxyz";
-        vector<string>ans;
-        int n = digits.size();
-        string str;
-        help(n,digits,0,ans,str,ma);
+        n = digits.size();
+        if(n==0)return  ans;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        rec(digits,0);
         return ans;
     }
 };
