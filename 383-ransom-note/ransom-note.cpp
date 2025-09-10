2391d91ffaae9d1ee6866c11814d2a4f70b78792
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-09-10 15:18:23 +0300

    Time: 7 ms (28.32%) | Memory: 12.1 MB (10.52%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int>mp1,mp2;
-        for(auto i:ransomNote)mp1[i]++;
-        for(auto i:magazine)mp2[i]++;
-        for(auto i:mp1)
-        {
-            if(mp2[i.first]<i.second)return false;
-        }
+        unordered_map<char,int>ransom,mag;
+        for(auto i:ransomNote)ransom[i]++;
+        for(auto i:magazine)mag[i]++;
+        for(auto i:mag)
+            if(i.second<ransom[i.first])
+                return false;
+        for(auto i:ransom)
+            if(i.second>mag[i.first])return false;
+        
         return true;
     }
 };
