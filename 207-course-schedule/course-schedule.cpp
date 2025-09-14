8c4e99d4f18c459eca7d032c29a89874882a9dde
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-09-14 15:54:47 +0300

    Time: 9 ms (29.29%) | Memory: 19.2 MB (74.82%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,39 +1,37 @@
+const int N = 2005;
 class Solution {
 public:
-    vector<vector<int>>adj;
-    vector<bool>vis;
-    vector<bool>parent;
-    bool cycled;
-    void dfs_cycled(int node)
+    vector<int>adj[N];
+    bool vis[N];
+    bool par[N];
+    bool dfsCycle(int x)
     {
-        vis[node] = true ;   
-        parent[node] = true;     
-        for (auto i: adj[node] )
+        vis[x] = true;
+        par[x] = true;
+        for(auto i:adj[x])
         {
-            if(parent[i])cycled = true; 
-            if (!vis[i])
-            {  
-                dfs_cycled(i);
+            if(par[i])
+                return true;
+            else if(!vis[i] && dfsCycle(i)) {
+
+                return true;
             }
         }
-        parent[node] = false;
+        par[x] = false;
+        return false;
+
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vis.resize(numCourses+2,0);
-        adj.resize(numCourses+2);
-        parent.resize(numCourses+2,0);
-        cycled = false;
         for(auto i:prerequisites)
         {
             adj[i[0]].push_back(i[1]);
         }
-        bool ans = true;
         for(int i=0;i<numCourses;i++)
         {
             if(!vis[i]){
-                dfs_cycled(i);
+                if(dfsCycle(i))return false;
             }
         }
-        return !cycled;
+        return true;
     }
 };
