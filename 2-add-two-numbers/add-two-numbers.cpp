ea38177180f3a280c57cf037e34c1cd5da0c4889
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-09-19 09:26:15 +0300

    Time: 0 ms (100.00%) | Memory: 77 MB (92.10%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,51 +11,57 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans = new ListNode(0);
-        ListNode* res = ans;
-        int rem = 0;
-        while(l1!=NULL &&l2!=NULL)
-        {
-            int sum = l1->val + l2->val;
-            sum+=rem;
-            if(sum>=10){
-                sum-=10;
+       ListNode* ans = new ListNode();
+       ListNode *res = ans; 
+       int rem = 0,sum = 0;
+       while(l1!=NULL && l2!=NULL)
+       {
+            sum = l1->val + l2->val;
+            sum +=rem;
+            rem = 0;
+            if(sum>=10)
+            {
+                sum -=10;
                 rem = 1;
             }
-            else rem = 0;
-            ans->next = new ListNode(sum);
+            ans->next = new ListNode();
+            ans->next->val = sum;
             ans = ans->next;
             l1 = l1->next;
             l2 = l2->next;
-        }
-        while(l1!=NULL)
-        {
-            int sum = l1->val;
-            sum+=rem;
-            cout<<sum<<' ';
-            if(sum>=10){
-                sum-=10;
+       }
+       while(l1!=NULL)
+       {
+            sum = l1->val;
+            sum +=rem;
+            rem = 0;
+            if(sum>=10)
+            {
+                sum -=10;
                 rem = 1;
             }
-            else rem = 0;
-            ans->next = new ListNode(sum);
+            ans->next = new ListNode();
+            ans->next->val = sum;
+            ans = ans->next;
             l1 = l1->next;
-            ans = ans->next;
-        }
-        while(l2!=NULL)
-        {
-            int sum = l2->val;
-            sum+=rem;
-            if(sum>=10){
-                sum-=10;
+       }
+       while(l2!=NULL)
+       {
+            sum = l2->val;
+            sum +=rem;
+            rem = 0;
+            if(sum>=10)
+            {
+                sum -=10;
                 rem = 1;
             }
-            else rem = 0;
-            ans->next = new ListNode(sum);
-            l2 = l2->next;
+            ans->next = new ListNode();
+            ans->next->val = sum;
             ans = ans->next;
-        }
-        if(rem)ans->next = new ListNode(rem);
-        return res->next;
+            l2 = l2->next;
+       }
+       if(rem)ans->next = new ListNode(rem);
+       return res->next;
+
     }
 };
