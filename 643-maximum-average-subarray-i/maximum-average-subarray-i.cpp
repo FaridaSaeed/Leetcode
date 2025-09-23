b4a050f4ff7d66df3d7a5c02685bf6e0e36a702e
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-09-23 16:17:26 +0300

    Time: 8 ms (6.56%) | Memory: 119 MB (11.33%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -6,13 +6,13 @@ public:
         for(int i=1;i<n;i++)
             pre[i]+=pre[i-1];
         int l = 0, r = l+k-1;
-        double ans = -1e5;
+        double ans = -1e9;
         while(r<n)
         {
-            double x = (pre[r]-(l?pre[l-1]:0))/(k*1.0);
+            double x = pre[r]-(l?pre[l-1]:0);
             ans = max(ans,x);
             l++,r++;
         }
-        return ans;
+        return ans/k;
     }
 };
