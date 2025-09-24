dc30d3789b2dd769a74ef6c7df920a57f19c8061
FaridaSaeed <119453240+FaridaSaeed@users.noreply.github.com>
2025-09-24 12:05:54 +0300

    Time: 0 ms (100.00%) | Memory: 9.4 MB (61.60%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     string decodeString(string s) {
+        string ans;
         stack<char>st;
         for(auto i:s)
         {
             if(i==']')
             {
+                string tmp;
+                while(st.top()!='[')
+                {
+                    tmp+=st.top();
+                    st.pop();
+                }
+                st.pop();
+                reverse(tmp.begin(),tmp.end());
                 string x;
-                while(!st.empty() && st.top()!='[')
+                while(!st.empty()&& st.top()-'0'>=0 && st.top()-'0'<=9)
                 {
                     x+=st.top();
                     st.pop();
                 }
-                if(st.top()=='[')
-                {
-                    st.pop();
-                    reverse(x.begin(),x.end());
-                    string num;
-                    while(!st.empty() && st.top()!='[' && !(st.top()>='a' && st.top()<='z'))
-                    {
-                        num+=st.top();
-                        st.pop();
-                    }
-                    reverse(num.begin(),num.end());
-                    int n = 0;
-                    if(num.size())
-                        n = stoi(num);
-                    string a;
-                    while(n--)a+=x;
-                    cout<<a<<' ';
-                    for(auto f:a)
-                        st.push(f);
-                }
+                reverse(x.begin(),x.end());
+                cout<<x<<' ';
+                int c = stoi(x);
+                x.clear();
+                while(c--)x+=tmp;
+                for(auto j:x)
+                    st.push(j);
             }
             else st.push(i);
         }
-        string ans;
-        while(!st.empty()){
+        while(!st.empty())
+        {
             ans+=st.top();
             st.pop();
         }
